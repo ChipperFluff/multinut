b637bdd7b65fd8e45eaeaff46aaaa82074781aca
Dominik Krenn <dominik.krenn@1tool.com>
2025-06-30 10:34:35 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chipi',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Chipperfluff',
+    author_email='i96774080@gmail.com',
+    description='A completely unnecessary multitool module.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ChipperFluff/chipi-tool',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
